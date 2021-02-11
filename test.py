8566c1c716199a4520fa7c37ca220722b4585f30
Rex Greenway <rexgreenway@gmail.com>
2021-02-11 13:12:57 +0000

    removed test file from repo

--- test.py
+++
@@ -1,9 +0,0 @@
-import polylatlib as pl
-
-# Drawing the shapes the wrong way????
-A = pl.Rectangle(2, 2, rotation=10)
-A.add_vertex("Origin", (0, 0), 10, "r")
-latA = A.generate_lattice_from_vectors(2, A.polygon_vectors)
-latA.add_vertex("Centre", (1, 1), 10, "r")
-
-latA.draw_shape()
