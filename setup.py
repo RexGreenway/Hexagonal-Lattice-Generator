0782d8aaf00ccad41830883317704a93f1148877
Rex Greenway <rexgreenway@gmail.com>
2021-03-26 14:51:41 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="polylatlib",
     url = "https://github.com/RexGreenway/polylatlib",
     packages=["polylatlib", "polylatlib.classes"],
-    version="1.0.2",
+    version="1.0.4",
     description="Python library for the generation of shapes and their lattices",
     author="Thomas Rex Greenway",
 )
